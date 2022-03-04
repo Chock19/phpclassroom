f96bd2a12ab535ed920c7f02ba3b9d9da949a77e
Chock19 <89768148+Chock19@users.noreply.github.com>
2022-03-04 21:03:51 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(shining.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(MiPancakeBold_1.woff);
+        src: url(MiPancakeThin_1.woff);
+        src: url(MiPancake_1.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นาย ศุภโชค ขจรศักดา เลขที่  9 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h>1นาย ศุภโชค ขจรศักดา เลขที่  9 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
